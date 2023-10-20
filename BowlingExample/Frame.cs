26d0c1aa1aade84ac7c4c846ce5b43614e52bde6
FilipJosifljevic <josifljevic.it44.2020@uns.ac.rs>
2023-10-20 13:57:56 +0200

    first setup

---
+++ BowlingExample/Frame.cs
@@ -0,0 +1,78 @@
+﻿namespace BowlingExample
+{
+    internal class Frame
+    {
+        private int attempt1 = 0, attempt2 = 0, bonus =0;
+        private bool Strike, Spare;
+        private static int total = 0;
+
+        public Frame()
+        {
+        }
+
+        public int getAttempt1()
+        {
+            return attempt1;
+        }
+        public int getAttempt2()
+        {
+            return attempt2;
+        }
+
+        public void setAttempt1(int attempt1)
+        {
+            this.attempt1 = attempt1;
+        }
+
+        public void setAttempt2(int attempt2)
+        {
+            this.attempt2 = attempt2;
+        }
+
+        public int getBonus()
+        {
+            return bonus;
+        }
+
+        public void setBonus(int bonus)
+        {
+            this.bonus = bonus;
+        }
+
+        public bool isStrike()
+        {
+            return Strike;
+        }
+
+        public void setStrike(bool isStrike)
+        {
+            this.Strike = isStrike;
+        }
+
+        public bool isSpare()
+        {
+            return Spare;
+        }
+
+        public void setSpare(bool isSpare)
+        {
+            this.Spare = isSpare;
+        }
+
+        public int getTotal(int total)
+        {
+            return total;
+        }
+
+        public void setTotal(int attempt1, int attempt2, int bonus)
+        {
+            total += attempt1 + attempt2 +bonus;
+        }
+
+        public String toString(int frameNumber)
+        {
+            return "Frame " + frameNumber.ToString() + ": [Attempt 1 -> " + attempt1.ToString() + ", Attempt 2 ->" + attempt2.ToString() + ", Bonus -> " + bonus.ToString() + ", Total -> " + total.ToString() +"]";
+        }
+
+    }
+}
---
+++ BowlingExample/Program.cs
@@ -0,0 +1,40 @@
+﻿using BowlingExample;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Frame[] frames = new Frame[10];
+        for (int i = 0; i < frames.Length; i++)
+        {
+            frames[i] = new Frame();
+            Console.WriteLine("Frame : " + (i + 1) + "\n");
+            Console.WriteLine("Attempt 1 : ");
+            frames[i].setAttempt1(Int32.Parse(Console.ReadLine()));
+            if (frames[i].getAttempt1() == 10)
+            {
+                frames[i].setStrike(true);
+                continue;
+            }
+            Console.WriteLine("Attempt 2 : ");
+            frames[i].setAttempt2(Int32.Parse(Console.ReadLine()));
+            if (frames[i].getAttempt1() + frames[i].getAttempt2() == 10)
+            {
+                frames[i].setSpare(true);
+            }
+            if (i > 0)
+            {
+                if (frames[i - 1].isSpare())
+                    frames[i - 1].setBonus(frames[i].getAttempt1());
+                if (frames[i - 1].isStrike())
+                    frames[i - 1].setBonus(frames[i].getAttempt1() + frames[i].getAttempt2());
+            }
+            
+            for(int j = 0; j < frames.Length;++j)
+            {
+                Console.WriteLine(frames[j].toString);
+            }
+            Console.WriteLine("\n\n");
+        }
+    }
+}
